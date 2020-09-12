d6c85808f96e6a7cb5d67a4aa721faa34024d30a
Clarisse BREANT <70048203+Clarisse-BREANT@users.noreply.github.com>
2020-09-12 19:02:57 +0400

    Add files via upload

--- TP_Grand_Raid/js/function_initialiser.js
+++ TP_Grand_Raid/js/function_initialiser.js
@@ -1,11 +1,6 @@
-function include(fileName){
-        document.write(<script type='text/javascript' src='+fileName+'></script>)
-}
-
 function initialiser() {
 
-        include(parcours.js);
-        include(marqueurs.js);
+        document.write("<div id='carte' style='width:100%; height:100%'></div>");
 
         var latlng = new google.maps.LatLng(-21.135111,55.2464294);
         var options = {
@@ -14,20 +9,23 @@ function initialiser() {
             mapTypeId: google.maps.MapTypeId.ROADMAP
          };
 
-         var carte = new google.maps.Map(document.getElementById("carte"), options);
- 
-                
-         var TraceParcoursGrandRaid =new google.maps.Polyline({
-             path: parcoursGrandRaid,
-             strokeColor: "#FF0000",
-             strokeOpacity: 1.0,
-             strokeWeight: 2                
-         });
+        var carte = new google.maps.Map(document.getElementById("carte"), options);
+
+        
+        drawMarqueurs(carte);
+
+        var parcoursGrandRaid = [];
+        getParcours(parcoursGrandRaid);
+
+        
+        var TraceParcoursGrandRaid =new google.maps.Polyline({
+            path: parcoursGrandRaid,
+            strokeColor: "#FF0000",
+            strokeOpacity: 1.0,
+            strokeWeight: 2                
+        });
 
         TraceParcoursGrandRaid.setMap(carte);
-
-        google.maps.event.addListener(marqueur, 'click', function() {
-             alert("Le marqueur a été cliqué.");//message d'alerte
-         });
+        
 
 }
